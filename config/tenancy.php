85c1c24c2abc8cd2d8a0fba293061f76718f2cf5
Abdelrhman Ahmed <abdelrhmanmt1@gmail.com>
2026-07-29 15:30:40 +0300

    technomasrsystem_tenant
    technomasrsystem_tenant

--- config/tenancy.php
+++ config/tenancy.php
@@ -63,7 +63,8 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'rwadsolu_tenant_',
+        'prefix' => 'technomasrsystem_tenant',
+        // 'prefix' => 'rwadsolu_tenant_',
         'suffix' => '',
 
         /**
