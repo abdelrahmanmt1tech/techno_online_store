e839e590aec388f7a8e5ff28367801e8b9ca0ffe
Mohamed Salah <mohamed.sala71996@gmail.com>
2026-08-02 12:59:16 +0300

    up

--- app/Http/Resources/Tenant/OrderItemResource.php
+++ app/Http/Resources/Tenant/OrderItemResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Tenant;
 
+use App\Helper\CurrencyHelper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -13,18 +14,20 @@ class OrderItemResource extends JsonResource
             'id' => $this->id,
             'product_name' => $this->product_name,
             'product_sku' => $this->product_sku,
-            'options' => $this->variant?->options->map(fn ($o) => [
+            'options' => $this->variant?->options->map(fn($o) => [
                 'name' => $o->variation->name ?? null,
                 'value' => $o->value,
             ]),
             'quantity' => $this->quantity,
             'unit_price' => $this->unit_price,
             'total_price' => $this->unit_price * $this->quantity,
+            'currency' => CurrencyHelper::getCurrency(),
+
             'product_image' => $this->whenLoaded('product', function () {
                 $media = $this->product->media->first();
 
                 return $media
-                    ? asset('storage/tenant'.tenant('id').'/'.$media->file)
+                    ? asset('storage/tenant' . tenant('id') . '/' . $media->file)
                     : null;
             }),
         ];
